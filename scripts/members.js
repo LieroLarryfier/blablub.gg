79991e10f32d4cb45c01814b9144865589728c50
LieroLarryfier <lierolarryfier@blablub.gg>
2016-02-11 13:22:04 +0100

    added squads, adjusted logo;

---
+++ scripts/members.js
@@ -0,0 +1,8 @@
+/**
+ * Created by LieroLarryfier on 17.12.2015.
+ */
+//TODO: server for filehandling
+
+$.getJSON("test.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
